1214b5882f7c8aa55b1f222100128951d9c6aa5a
Shawn Wallis <shawn.p.wallis@gmail.com>
2024-09-09 15:41:01 -0400

    feat: radio pillbox style

--- radio/style.go
+++ radio/style.go
@@ -19,7 +19,32 @@ func DefaultStyles(vertical bool) Styles {
 	return s
 }
 
+func DefaultPillboxStyles(vertical bool) Styles {
+	s := Styles{
+		Button: gloss.NewStyle().
+			Border(DefaultPillboxBorder, true).
+			Padding(0, 2, 0, 2),
+	}
+
+	if vertical {
+		s.Button = s.Button.UnsetPadding()
+	}
+
+	return s
+}
+
 var (
 	DefaultUnfocusedColor          = gloss.AdaptiveColor{Light: "#3a3a3a", Dark: "#b0b0b0"}
 	DefaultActiveTabIndicatorColor = gloss.AdaptiveColor{Light: "#bb99fe", Dark: "#997bf6"}
+
+	DefaultPillboxBorder = gloss.Border{
+		Top:         "─",
+		Bottom:      "─",
+		Left:        "│",
+		Right:       "│",
+		TopLeft:     "╭",
+		TopRight:    "╮",
+		BottomLeft:  "╰",
+		BottomRight: "╯",
+	}
 )
